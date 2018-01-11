880406a13e3e498801218d170ae37b6a28dce9f9
cliveportman <clive@cliveportman.co.uk>
2018-01-11 15:10:47 +0000

    Initial commit

---
+++ SalesFiguresPlugin.php
@@ -0,0 +1,39 @@
+<?php
+namespace Craft;
+
+/**
+ * SalesFigures plugin
+ *
+ * @author    Clive Portman <clive@cliveportman.co.uk>
+ * @copyright Copyright (c) 2016, Clive Portman.
+ * @license   MIT
+ * @version   0.1
+ */
+
+class ProductsCalendarPlugin extends BasePlugin
+{
+    function getName()
+    {
+        return Craft::t('Sales Figures');
+    }
+    function getDescription()
+    {
+        return Craft::t('Returns total sales of purchasable within a date range.');
+    }
+
+    function getVersion()
+    {
+        return '0.1';
+    }
+
+    function getDeveloper()
+    {
+        return 'Clive Portman';
+    }
+
+    function getDeveloperUrl()
+    {
+        return 'https://cliveportman.co.uk';
+    }
+
+}
