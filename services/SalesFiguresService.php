dbec8ef3810c22618e1d87d9a06ee1da98464f16
cliveportman <clive@cliveportman.co.uk>
2018-01-12 12:08:28 +0000

    Switching to providing productIds in the template

--- services/SalesFiguresService.php
+++ services/SalesFiguresService.php
@@ -11,20 +11,13 @@ class SalesFiguresService extends BaseApplicationComponent
     	// REQUIRES startDate AND endDate, FORMATTED AS YYYY-MM-DD
         $startDate = $options['startDate'];
         $endDate = $options['endDate'];
-        $classId = $options['classId'];
+        $productIds = $options['productIds'];
 
         // START BY GETTING THE PRODUCTS LINKED TO THE CLASS
-        $products = craft()->salesFigures->getProductsRelatedToClass($startDate, $endDate, $classId);
-
-        // USE THESE PRODUCTS TO CREATE A LIST OF VARIANT IDS
-        $variantIds = [];
-        foreach ($products as $product) {
-            echo $product->title;
-            array_push($variantIds, $product->defaultVariant->id);
-        }
+        $variantIds = craft()->salesFigures->getVariants($productIds);
 
         // GET THE ORDERS FOR THE PERIOD IN QUESTION
-        $orders = craft()->salesFigures->getOrdersWithinRange($startDate, $endDate);
+        $orders = craft()->salesFigures->getOrdersWithinRange($startDate, $endDate, $variantIds);
 
         $numberOfSales = 0;
         $valueOfSales = 0;
@@ -32,7 +25,6 @@ class SalesFiguresService extends BaseApplicationComponent
         // LOOP THROUGH EACH LINEITEM IN EACH ORDER
         foreach ($orders as $order) {
         foreach ($order->getLineItems() as $lineItem) {
-            echo $lineItem->purchasableId . "/";
             if (in_array($lineItem->purchasableId, $variantIds)) {
                 $numberOfSales = $numberOfSales + $lineItem->qty;
                 $valueOfSales = $valueOfSales + $lineItem->total;
@@ -44,36 +36,35 @@ class SalesFiguresService extends BaseApplicationComponent
         $returnObject = new \stdClass;
         $returnObject->totalSalesNumber = $numberOfSales;
         $returnObject->totalSalesValue = $valueOfSales;
-        $returnObject->classId = $classId;
         $returnObject->variantIds = $variantIds;
 
         return $returnObject;
                 
     }
 
-    public function getProductsRelatedToClass($startDate, $endDate, $classId) {
+    public function getVariants($productIds) {
 
-        // START BY GETTING THE ENTRY FROM ITS ID
-        $class = craft()->entries->getEntryById($classId);
-        echo $class->title;
-        // THEN GET THE PRODUCTS
+        // GET THE PRODUCTS
         $criteria = craft()->elements->getCriteria('Commerce_Product');
-        $criteria->date = array('and', '>= ' . $startDate, '<= ' . $endDate);
-        $criteria->relatedTo(array(
-            'targetElement' => $class,
-            'field' => 'class'
-        ));
+        $criteria->id = $productIds;
         $criteria->limit = NULL;
         $products = $criteria->find();
 
-        return $products;  
+        $variantIds = [];
+        foreach ($products as $product) {
+            array_push($variantIds, $product->defaultVariant->id);
+        }
+
+        return $variantIds;  
 
     }
 
-    public function getOrdersWithinRange($startDate, $endDate) {
+    public function getOrdersWithinRange($startDate, $endDate, $variantIds) {
 
         $criteria = craft()->elements->getCriteria('Commerce_Order');
         $criteria->dateOrdered = array('and', '>= ' . $startDate, '<= ' . $endDate);
+        $criteria->hasPurchasables = $variantIds;
+        $criteria->limit = NULL;
         $orders =  $criteria->find();
         return $orders;  
 
