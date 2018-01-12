d336b94036f7b08acc7f91ac8fd446ba6e25fae8
cliveportman <clive@cliveportman.co.uk>
2018-01-12 11:26:12 +0000

    Working service
    But relationTo is being ignored.

--- services/SalesFiguresService.php
+++ services/SalesFiguresService.php
@@ -11,36 +11,63 @@ class SalesFiguresService extends BaseApplicationComponent
     	// REQUIRES startDate AND endDate, FORMATTED AS YYYY-MM-DD
         $startDate = $options['startDate'];
         $endDate = $options['endDate'];
+        $classId = $options['classId'];
 
-        // $variants = craft()->salesFigures->getVariants();
+        // START BY GETTING THE PRODUCTS LINKED TO THE CLASS
+        $products = craft()->salesFigures->getProductsRelatedToClass($startDate, $endDate, $classId);
 
+        // USE THESE PRODUCTS TO CREATE A LIST OF VARIANT IDS
+        $variantIds = [];
+        foreach ($products as $product) {
+            echo $product->title;
+            array_push($variantIds, $product->defaultVariant->id);
+        }
 
-
-        /*
-        RETURNS
-        an array of objects
-        each object has a purchasableId as it's index
-        and contains the total number of sales and total sales value
-        of the purchasable
-        */
-
-        // START BY FETCHING THE ORDERS WITHIN THE DATE RANGE
+        // GET THE ORDERS FOR THE PERIOD IN QUESTION
         $orders = craft()->salesFigures->getOrdersWithinRange($startDate, $endDate);
 
-        // GET ALL PURCHASED VARIANTS FROM THE ORDERS
-        $purchasedVariants = craft()->salesFigures->getPurchasedVariants($orders);
+        $numberOfSales = 0;
+        $valueOfSales = 0;
 
-        // SET THE RETURN ARRAY
-        $return = [];
+        // LOOP THROUGH EACH LINEITEM IN EACH ORDER
+        foreach ($orders as $order) {
+        foreach ($order->getLineItems() as $lineItem) {
+            echo $lineItem->purchasableId . "/";
+            if (in_array($lineItem->purchasableId, $variantIds)) {
+                $numberOfSales = $numberOfSales + $lineItem->qty;
+                $valueOfSales = $valueOfSales + $lineItem->total;
+            }
+        }
+        }
 
+        // PREPARE THE RETURN OBJECT
+        $returnObject = new \stdClass;
+        $returnObject->totalSalesNumber = $numberOfSales;
+        $returnObject->totalSalesValue = $valueOfSales;
+        $returnObject->classId = $classId;
+        $returnObject->variantIds = $variantIds;
 
-        // LOOP THROUGH THE VARIANTS
-
-        // IF THEY AREN'T IN THE RETURN ARRAY, ADD THEM
-        // IF THEY ARE, UPDATE THE RETURN ARRAY WITH THE ADDITIONAL SALE NUMBERS AND VALUES
-
-        return $purchasedVariants;
+        return $returnObject;
                 
+    }
+
+    public function getProductsRelatedToClass($startDate, $endDate, $classId) {
+
+        // START BY GETTING THE ENTRY FROM ITS ID
+        $class = craft()->entries->getEntryById($classId);
+        echo $class->title;
+        // THEN GET THE PRODUCTS
+        $criteria = craft()->elements->getCriteria('Commerce_Product');
+        $criteria->date = array('and', '>= ' . $startDate, '<= ' . $endDate);
+        $criteria->relatedTo(array(
+            'targetElement' => $class,
+            'field' => 'class'
+        ));
+        $criteria->limit = NULL;
+        $products = $criteria->find();
+
+        return $products;  
+
     }
 
     public function getOrdersWithinRange($startDate, $endDate) {
@@ -49,59 +76,6 @@ class SalesFiguresService extends BaseApplicationComponent
         $criteria->dateOrdered = array('and', '>= ' . $startDate, '<= ' . $endDate);
         $orders =  $criteria->find();
         return $orders;  
-
-    }
-
-    public function getPurchasedVariants($orders) {
-
-        // CREATE AN ARRAY TO HOLD ALL OF THE LINEITEMS' PURCHASABLES
-        $purchasedVariants = [];
-
-        // LOOP THROUGH EACH LINEITEM IN EACH ORDER
-        foreach ($orders as $order) {
-        	foreach ($order->getLineItems() as $lineItem) {
-	            if ($lineItem->purchasableId) {
-	            	array_push($purchasedVariants, $lineItem->purchasable);
-	            }
-	        }
-        }
-
-        return $purchasedVariants;
-
-    }
-
-    public function getAllVariants() {
-
-    	/********************
-    	NOT CURRENTLY USING
-    	WE STARTED THIS PLUGIN BY LOOPING THROUGH THE VARIANTS TO CREATE A LIST,
-    	THEN CROSS-CHECKING THEM WITH ORDERS FROM THE SELECTED PERIOD.
-    	BUT THE NUMBER OF VARIANTS MAKES THIS TOO INTENSIVE.
-    	INSTEAD WE'RE TRYING LOOPING THROUGH THE ORDERS WITHIN THE RANGE.
-    	********************/
-
-        // START BY CREATING AN ARRAY OF ALL THE COMMERCE VARIANTS
-        $criteria = craft()->elements->getCriteria('Commerce_Variant');
-        $criteria->limit = NULL;
-        $variants =  $criteria->find();
-
-        // AND ANOTHER ARRAY OF VARIANTS WE WANT TO RETURN
-        $simplifiedVariants = [];
-
-        // LOOP THROUGH THE COMMERCE VARIANTS
-        foreach ($variants as $variant) {
-            
-            // AND CREATE A SIMPLIFIED VARIANT OBJECT BEFORE ADDING IT TO THE RETURN ARRAY
-            $simplifiedVariant = new \stdClass;
-            $simplifiedVariant->title = $variant->id;
-            $simplifiedVariant->id = $variant->title;
-            $simplifiedVariant->salesNumber = 0;
-            $simplifiedVariant->salesValue = 0;
-            array_push($simplifiedVariants, $simplifiedVariant);
-
-        }
-
-        return $simplifiedVariants;
 
     }
 
