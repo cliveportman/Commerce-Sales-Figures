547f920b8578eac9695ad4ce76ef743bfaacc026
cliveportman <clive@cliveportman.co.uk>
2018-03-07 11:16:57 +0000

    Updating some function names

--- services/SalesFiguresService.php
+++ services/SalesFiguresService.php
@@ -5,7 +5,7 @@ namespace Craft;
 class SalesFiguresService extends BaseApplicationComponent
 {
 
-    public function getSales($options)
+    public function getSalesByProductIds($options)
     {
 
     	// REQUIRES startDate AND endDate, FORMATTED AS YYYY-MM-DD
@@ -21,12 +21,25 @@ class SalesFiguresService extends BaseApplicationComponent
 
         $numberOfSales = 0;
         $valueOfSales = 0;
+        $unsoldSpaces = 0;
+
+        // WHEN A LINEITEM HAS THE QTY REDUCED USING THE STOCKREPLENISHER PLUGIN
+        // WE ADD A NOTE 'ITEM CANCELLED' BUT WE HAVE NO RECORD OF THE ORIGINAL QTY.
+        // THE LINEITEM TOTAL IS UPDATED SO CAN BE COMPARED WITH THE
+        // ORDER TOTAL (THAT ISN'T UPDATED) TO FIND THE VALUE OF THE CANCELLATIONS
+        // BUT FINDING THE NUMBER OF CANCELLATIONS WILL BE UNRELIABLE WHEN MORE THAN
+        // ONE LINE ITEM IS UPDATE
+        // RECOMMEND CREATING A CUSTOM ORDER FIELD CALLED 'CANCELLATIONS' THAT STORES
+        // A JSON OBJECT OR TABLE CONTAINING DETAILS OF A CANCELLATION, E.G.
+        // ONE ROW PER CANCELLATION REQUEST WITH LINEITEM ID, PURCHASABLE ID, NUMBER CANCELLED, VALUE CANCELLED
+        // THIS COULD BE MANUALLY UPDATED FOR PREVIOUS CANCELLATIONS
 
         // LOOP THROUGH EACH LINEITEM IN EACH ORDER
         foreach ($orders as $order) {
         foreach ($order->getLineItems() as $lineItem) {
             if (in_array($lineItem->purchasableId, $variantIds)) {
                 $numberOfSales = $numberOfSales + $lineItem->qty;
+                $unsoldSpaces = $unsoldSpaces + $lineItem->purchasable->stock;
                 $valueOfSales = $valueOfSales + $lineItem->total;
             }
         }
@@ -36,6 +49,7 @@ class SalesFiguresService extends BaseApplicationComponent
         $returnObject = new \stdClass;
         $returnObject->totalSalesNumber = $numberOfSales;
         $returnObject->totalSalesValue = $valueOfSales;
+        $returnObject->unsoldSpaces = $unsoldSpaces;
         $returnObject->variantIds = $variantIds;
 
         return $returnObject;
@@ -67,6 +81,29 @@ class SalesFiguresService extends BaseApplicationComponent
         $criteria->limit = NULL;
         $orders =  $criteria->find();
         return $orders;  
+
+    }
+
+    public function getTotalSales($options)
+    {
+
+        // REQUIRES startDate AND endDate, FORMATTED AS YYYY-MM-DD
+        $startDate = $options['startDate'];
+        $endDate = $options['endDate'];
+
+        // GET ALL THE ORDERS WITHIN THE TWO DATES
+        $criteria = craft()->elements->getCriteria('Commerce_Order');
+        $criteria->dateOrdered = array('and', '>= ' . $startDate, '<= ' . $endDate);
+        $criteria->orderStatus = 1;
+        $criteria->limit = NULL;
+        $orders =  $criteria->find();
+
+        $salesTotal = 0;
+        foreach ($orders as $order) {
+            $salesTotal = $salesTotal + $order->totalPrice;
+        }
+
+        return $salesTotal;
 
     }
 
--- variables/SalesFiguresVariable.php
+++ variables/SalesFiguresVariable.php
@@ -5,11 +5,19 @@ namespace Craft;
 class SalesFiguresVariable
 {
 
-    public function getSales($options = array()) {
+    public function getSalesByProduct($options = array()) {
 
     	// REQUIRED OPTIONS ARE startDate AND endDate, FORMATTED AS YYYY-MM-DD
-        $sales = craft()->salesFigures->getSales($options);
+        $sales = craft()->salesFigures->getSalesByProductIds($options);
         return $sales;
+
+    }
+
+    public function getTotalSales($options = array()) {
+
+    	// REQUIRED OPTIONS ARE startDate AND endDate, FORMATTED AS YYYY-MM-DD
+        $salesTotal = craft()->salesFigures->getTotalSales($options);
+        return $salesTotal;
 
     }
 
